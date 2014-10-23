231d4594168010d548ff9ed2a66ce4d56d5bc824
Koji Shimba <bojovs@gmail.com>
2014-10-23 22:09:59 +0900

    ローカル環境ではGitとAtomの設定ファイルだけ使う

--- install.rb
+++ install.rb
@@ -1,28 +1,9 @@
 #!/usr/bin/env ruby
 
 dotfiles = [
-  '.ackrc',
-  '.aliases',
   '.atom',
-  '.bash_profile',
-  '.bashrc',
-  '.earthquake',
-  '.envrc',
-  '.gemrc',
   '.gitconfig',
-  '.gitignore_global',
-  '.gvimrc',
-  '.pryrc',
-  '.railsrc',
-  '.spring.rb',
-  '.tmux.conf',
-  '.vim',
-  '.vimrc',
-  '.zlogin',
-  '.zsh',
-  '.zshrc',
-  'bin',
-  'Brewfile'
+  '.gitignore_global'
 ]
 
 dotfiles.each do |file|
